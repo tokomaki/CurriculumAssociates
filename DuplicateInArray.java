8a63778af92535d13d8355885f23548cfcf58385
jusvic <justin.victoria09@gmail.com>
2020-12-07 03:45:20 -0500

    Modify program to take input dynamically and intelligently ignore negatives

--- DuplicateInArray.java
+++ DuplicateInArray.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class DuplicateInArray {
 
@@ -7,7 +9,7 @@ public class DuplicateInArray {
         // Create a dynamic array to store the numbers that occur more than once
         List<Integer> seen = new ArrayList<>();
         // Gather the max number from the array to determine the size of the boolean array
-        int max = -1;
+        int max = Integer.MIN_VALUE;
         for (int i : arr) {
             if (i > max) {
                 max = i;
@@ -27,9 +29,27 @@ public class DuplicateInArray {
         return seen;
     }
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int[] arr;
-        arr = new int[]{1, 2, 3, 2, 4, 5, 6, 9, 8, 9, 8, 11, 100, 74, 100};
-        System.out.println(duplicate(arr).toString());
+        System.out.println("Please input the size:");
+        int size = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] q = new int[size];
+
+        System.out.println("Please input the numbers for the array separated by spaces:");
+        String[] qItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < size; i++) {
+            int qItem = Integer.parseInt(qItems[i].replaceAll("-", ""));
+            q[i] = qItem;
+        }
+
+        System.out.println(Arrays.toString(q));
+        System.out.println(duplicate(q));
+
+        scanner.close();
     }
 }
